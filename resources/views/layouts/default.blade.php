b24f1d3fd946bfa29d6904a6d9dcfb10a0d62cde
fnn <17888845985@163.com>
2017-08-14 14:17:36 +0000

    Finish Static Pages

---
+++ resources/views/layouts/default.blade.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+<head><title>@yield('title')---Laravel5.5-dev</title></head>
+<body>
+
+@yield('content')
+</body>
+
+</html>
---
+++ resources/views/static_pages/help.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.default')
+@section('title','帮助')
+@section('content')
+<h1>What can I do for you?</h1>
+@stop
